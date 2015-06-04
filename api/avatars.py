d434fb4b051cc0a0fb0ddf26f2d91f19d5a07dee
Michael Sondergaard <msond@cs.au.dk>
2015-06-04 18:13:58 +0200

    Allow avatar upload

--- api/avatars.py
+++ api/avatars.py
@@ -1,7 +1,33 @@
+import json
+import os
+from peewee import IntegrityError
+from werkzeug.utils import secure_filename
 from api import *
-
 from db.faf_orm import Avatar
 
-@app.route("/avatar")
-def list_avatars():
-    return [avatar.dict() for avatar in Avatar.select()]
+from flask import request
+
+@app.route("/avatar", methods=['GET', 'POST'])
+def avatars():
+    if request.method == 'POST':
+        file = request.files['file']
+        filename = secure_filename(file.filename)
+        file.save(os.path.join(app.config['AVATAR_FOLDER'], filename))
+
+        try:
+            avatar = Avatar.create(url=app.config['AVATAR_URL'], tooltip=request.form['tooltip'])
+            avatar.save()
+        except IntegrityError:
+            return json.dumps(dict(error="Avatar already exists")), 400
+
+        return avatar.dict()
+
+    else:
+        return [avatar.dict() for avatar in Avatar.select()]
+
+@app.route("/avatar/<int:id>", methods=['GET', 'PUT'])
+def avatar(id):
+    if request.method == 'GET':
+        return Avatar.get(Avatar.id == id).dict()
+    elif request.method == 'PUT':
+        raise NotImplemented('')
