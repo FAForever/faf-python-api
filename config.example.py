214ab138d531dcaa353cdeaf22e98f4fd54ad7bc
Sheeo <sheeo@faforever.com>
2015-08-26 12:27:38 +0200

    Add missing config key

--- config.example.py
+++ config.example.py
@@ -14,5 +14,6 @@ HOST_NAME = 'dinosaur.bob'
 AVATAR_FOLDER = 'avatars'
 AVATAR_URL = 'http://content.faforever.com/faf/avatars'
 
+GITHUB_USER = 'some-user'
 GITHUB_TOKEN = 'some token'
 GITHUB_SECRET = 'some-secret'
