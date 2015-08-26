273868ef9fcbb8bf64e7f2436d6cdaec6fd7ad6f
Sheeo <sheeo@faforever.com>
2015-08-26 22:29:21 +0200

    Update example config

--- config.example.py
+++ config.example.py
@@ -3,13 +3,11 @@ Configuration file for the FAForever Python Web Api
 """
 
 DATABASE = dict(
-    engine=  "playhouse.pool.PooledMySQLDatabase",
-    name=    "bob",
-    user=    "the",
-    passwd=  "dinosaur",
-    max_connections= 32, stale_timeout=600)
+    db="faf_test",
+    user="root",
+    password="")
 
-HOST_NAME = 'dinosaur.bob'
+HOST_NAME = 'dev.faforever.com'
 
 AVATAR_FOLDER = 'avatars'
 AVATAR_URL = 'http://content.faforever.com/faf/avatars'
