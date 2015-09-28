bb5f39b02f314b7c0e8a1c3026f9b391b2689a37
Dragonfire <bastian616-git@yahoo.de>
2015-09-28 13:47:24 +0200

    Allow CORS for clanapp http://www.wikiwand.com/en/Same-origin_policy#/Cross-Origin_Resource_Sharing

--- api/__init__.py
+++ api/__init__.py
@@ -76,3 +76,10 @@ import api.games
 import api.mods
 import api.github
 import api.clans
+
+@app.after_request
+def after_request(response):
+  response.headers.add('Access-Control-Allow-Origin', '*')
+  response.headers.add('Access-Control-Allow-Headers', 'Content-Type,Authorization')
+  response.headers.add('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE')
+  return response
