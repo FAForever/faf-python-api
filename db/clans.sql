262ee904a3a4a8a0f906139e487ca6d521a20e5f
Dragonfire <bastian616-git@yahoo.de>
2015-09-30 02:44:02 +0200

    Add more data to clan resource * leader * member count

--- db/clans.sql
+++ db/clans.sql
@@ -11,10 +11,20 @@ VIEW `clan_list` AS
         `fafclans`.`clans_list`.`clan_founder_id` AS `clan_founder_id`,
         `fafclans`.`clans_list`.`clan_desc` AS `clan_desc`,
         `fafclans`.`clans_list`.`create_date` AS `create_date`,
-        `faf_test`.`login`.`login` AS `founder_name`
+        `founder`.`login` AS `founder_name`,
+        `fafclans`.`clan_leader`.`player_id` AS `leader_id`,
+        `leader`.`login` AS `leader_name`,
+        (SELECT 
+                COUNT(0)
+            FROM
+                `faf_test`.`clan_members`
+            WHERE
+                (`clan_members`.`clan_id` = `fafclans`.`clans_list`.`clan_id`)) AS `member_count`
     FROM
-        (`fafclans`.`clans_list`
-        LEFT JOIN `faf_test`.`login` ON ((`fafclans`.`clans_list`.`clan_founder_id` = `faf_test`.`login`.`id`)));
+        (((`fafclans`.`clans_list`
+        LEFT JOIN `faf_test`.`login` `founder` ON ((`fafclans`.`clans_list`.`clan_founder_id` = `founder`.`id`)))
+        LEFT JOIN `fafclans`.`clan_leader` ON ((`fafclans`.`clans_list`.`clan_id` = `fafclans`.`clan_leader`.`clan_id`)))
+        LEFT JOIN `faf_test`.`login` `leader` ON ((`fafclans`.`clan_leader`.`player_id` = `leader`.`id`)));
 
 CREATE 
     ALGORITHM = UNDEFINED 
