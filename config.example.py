e40b973fddc38e70b770369d3ccc28ab9777906f
Sheeo <sheeo@faforever.com>
2015-09-30 22:03:12 +0200

    Add Dockerfile, setup config to use env vars

--- config.example.py
+++ config.example.py
@@ -1,17 +1,26 @@
 """
 Configuration file for the FAForever Python Web Api
 """
+import os
 
 DATABASE = dict(
-    db="faf_test",
-    user="root",
-    password="")
+    db=os.getenv("FAF_DB_NAME", "faf_test"),
+    user=os.getenv("FAF_DB_USER", "root"),
+    password=os.getenv("FAF_DB_PW", ""),
+    host=os.getenv("FAF_DB_HOST", "127.0.0.1"))
 
-HOST_NAME = 'dev.faforever.com'
+HOST_NAME = os.getenv("FAF_API_HOSTNAME", 'dev.faforever.com')
 
-AVATAR_FOLDER = 'avatars'
-AVATAR_URL = 'http://content.faforever.com/faf/avatars'
+ENVIRONMENT = os.getenv("FAF_API_ENVIRONMENT", 'testing')
+
+REPO_PATHS = {
+    "api": '.',
+    "patchnotes": '/opt/dev/www/patchnotes'
+}
+
+GAME_DEPLOY_PATH = '/opt/dev/www/content/faf/updaterNew'
 
 GITHUB_USER = 'some-user'
-GITHUB_TOKEN = 'some token'
-GITHUB_SECRET = 'some-secret'
+GITHUB_TOKEN = 'some-token'
+
+AUTO_DEPLOY = ['patchnotes']
