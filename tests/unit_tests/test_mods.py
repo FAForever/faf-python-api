cab45c1a52d62841d689fa8a6f2b53eef78b8e1f
Michel Jung <michel.jung89@gmail.com>
2015-11-14 23:42:22 +0100

    Adding API for mod upload

--- tests/unit_tests/test_mods.py
+++ tests/unit_tests/test_mods.py
@@ -1,4 +1,5 @@
 import json
+from io import BytesIO
 from flask import Response
 
 
@@ -17,3 +18,29 @@ def test_get_mods(test_client):
     for item in data:
         assert 'type' in item
 
+
+def test_mods_upload(test_client, app, tmpdir):
+    upload_dir = tmpdir.mkdir("map_upload")
+    app.config['MOD_UPLOAD_PATH'] = upload_dir.strpath
+    response = test_client.post('/mods/upload',
+                                data={'file': (BytesIO('my file contents'.encode('utf-8')), 'mod_name.zip')})
+
+    assert response.status_code == 200
+    assert 'ok' == response.get_data(as_text=True)
+
+    with open(upload_dir.join('mod_name.zip').strpath, 'r') as file:
+        assert file.read() == 'my file contents'
+
+
+def test_mods_upload_no_file_results_400(test_client, app, tmpdir):
+    response = test_client.post('/mods/upload')
+
+    assert response.status_code == 400
+    assert json.loads(response.get_data(as_text=True))['message'] == 'No file has been provided'
+
+
+def test_mods_upload_txt_results_400(test_client, app, tmpdir):
+    response = test_client.post('/mods/upload', data={'file': (BytesIO('1'.encode('utf-8')), 'mod_name.txt')})
+
+    assert response.status_code == 400
+    assert json.loads(response.get_data(as_text=True))['message'] == 'Invalid file extension'
