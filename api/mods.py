7b0212cebdb210d23757e63970210504b81e3ab9
Michel Jung <michel.jung89@gmail.com>
2015-11-16 17:29:57 +0100

    Add API to list mods

--- api/mods.py
+++ api/mods.py
@@ -1,12 +1,13 @@
 import os
 
-from flask import request, url_for
-from werkzeug.utils import secure_filename, redirect
+from flask import request
+from werkzeug.utils import secure_filename
 
 import faf.db as db
 from api import app, InvalidUsage
 
 ALLOWED_EXTENSIONS = {'zip'}
+MODS_PER_PAGE = 100
 
 
 @app.route('/mods/upload', methods=['POST'])
@@ -15,7 +16,7 @@ def mods_upload():
     if not file:
         raise InvalidUsage("No file has been provided")
 
-    if not allowed_file(file.filename):
+    if not file_allowed(file.filename):
         raise InvalidUsage("Invalid file extension")
 
     filename = secure_filename(file.filename)
@@ -24,22 +25,75 @@ def mods_upload():
 
 
 @app.route('/mods')
-def get_mods():
-    with db.connection.cursor() as cursor:
-        cursor.execute('SELECT name from table_mod')
-        mods = []
-        while cursor.rowcount > 1:
-            mods.append(cursor.fetchone())
-    return {
-        'data': [
-            {
-                'type': 'mods',
-                'id': 1
+def mods():
+    order_column = request.values.get('order_column', 'likes')
+    if order_column not in {'likes', 'plays', 'create_time'}:
+        raise InvalidUsage("Invalid order column")
+
+    order = request.values.get('order', 'ASC')
+    if order.lower() not in {'asc', 'desc'}:
+        raise InvalidUsage("Invalid order")
+
+    max_items = int(request.values.get('max', MODS_PER_PAGE))
+    if max_items > MODS_PER_PAGE:
+        raise InvalidUsage("Invalid max")
+
+    page = int(request.values.get('page', 1))
+    if page < 1:
+        raise InvalidUsage("Invalid page")
+
+    offset = (page - 1) * max_items
+    limit = max_items
+
+    with db.connection:
+        cursor = db.connection.cursor(db.pymysql.cursors.DictCursor)
+        cursor.execute("""
+            SELECT
+                uid as id,
+                name,
+                description,
+                version,
+                author,
+                ui as is_ui,
+                UNIX_TIMESTAMP(date) as create_time,
+                downloads,
+                likes,
+                played as plays,
+                filename,
+                icon as icon_filename,
+                ranked as is_ranked
+            FROM table_mod
+            ORDER BY {} {}
+            LIMIT %(offset)s, %(limit)s
+        """.format(order_column, order), dict(offset=offset, limit=limit))
+
+        result = cursor.fetchall()
+
+    data = []
+
+    for row in result:
+        data.append({
+            'type': 'mod',
+            'id': row['id'],
+            'attributes': {
+                'name': row['name'],
+                'description': row['description'],
+                'version': row['version'],
+                'author': row['author'],
+                'downloads': row['downloads'],
+                'likes': row['likes'],
+                'plays': row['plays'],
+                'filename': row['filename'],
+                'icon_filename': row['icon_filename'],
+                'is_ui': bool(row['is_ui']),
+                'is_ranked': bool(row['is_ranked']),
+                'create_time': row['create_time']
             }
-        ]
-    }
+        })
+
+    return {'data': data}
 
 
-def allowed_file(filename):
+def file_allowed(filename):
     return '.' in filename and \
            filename.rsplit('.', 1)[1] in ALLOWED_EXTENSIONS
--- tests/unit_tests/test_mods.py
+++ tests/unit_tests/test_mods.py
@@ -1,22 +1,138 @@
 import json
 from io import BytesIO
-from flask import Response
+import pytest
+import sys
+
+from faf import db
 
 
-def test_get_mods(test_client):
+@pytest.fixture
+def mods(request):
+    with db.connection:
+        cursor = db.connection.cursor()
+        cursor.execute("TRUNCATE TABLE table_mod")
+        cursor.execute("""INSERT INTO table_mod
+        (uid, name, version, author, ui, big, small, date, description, filename, icon, likes, likers) VALUES
+        ('mod-1', 'a', '1', 'author1', 0, 0, 0, FROM_UNIXTIME(1), '', 'mod1.zip', 'mod1.png', 100, x'00'),
+        ('mod-2', 'b', '2', 'author2', 0, 0, 0, FROM_UNIXTIME(2), '', 'mod2.zip', 'mod2.png', 200, x'00'),
+        ('mod-3', 'c', '3', 'author3', 0, 0, 0, FROM_UNIXTIME(3), '', 'mod3.zip', 'mod3.png', 300, x'00')""")
+
+    def finalizer():
+        with db.connection:
+            cursor = db.connection.cursor()
+            cursor.execute("TRUNCATE TABLE table_mod")
+
+    request.addfinalizer(finalizer)
+
+
+def test_mods(test_client, mods):
     response = test_client.get('/mods')
 
-    assert isinstance(response, Response)
     assert response.status_code == 200
     assert response.content_type == 'application/vnd.api+json'
 
-    response = json.loads(response.data.decode())
-    assert 'data' in response
+    result = json.loads(response.data.decode('utf-8'))
+    assert 'data' in result
+    assert len(result['data']) > 0
 
-    data = response['data']
-
-    for item in data:
+    for item in result['data']:
         assert 'type' in item
+
+
+def test_mods_max(test_client, mods):
+    response = test_client.get('/mods?max=1')
+
+    assert response.status_code == 200
+    assert response.content_type == 'application/vnd.api+json'
+
+    result = json.loads(response.data.decode('utf-8'))
+    assert 'data' in result
+    assert len(result['data']) == 1
+
+
+def test_mods_invalid_max(test_client, mods):
+    response = test_client.get('/mods?max=101')
+
+    assert response.status_code == 400
+    assert json.loads(response.get_data(as_text=True))['message'] == 'Invalid max'
+
+
+def test_mods_page(test_client, mods):
+    response = test_client.get('/mods?max=1&page=2')
+
+    assert response.status_code == 200
+    assert response.content_type == 'application/vnd.api+json'
+
+    result = json.loads(response.data.decode('utf-8'))
+    assert 'data' in result
+    assert len(result['data']) == 1
+    assert result['data'][0]['attributes']['name'] == 'b'
+
+
+def test_mods_invalid_page(test_client, mods):
+    response = test_client.get('/mods?page=-1')
+
+    assert response.status_code == 400
+    assert json.loads(response.get_data(as_text=True))['message'] == 'Invalid page'
+
+
+def test_mods_sort_by_create_time(test_client, mods):
+    response = test_client.get('/mods?order_column=create_time')
+
+    assert response.status_code == 200
+    assert response.content_type == 'application/vnd.api+json'
+
+    result = json.loads(response.data.decode('utf-8'))
+    assert len(result['data']) > 0
+
+    previous_create_time = 0
+    for item in result['data']:
+        assert item['attributes']['create_time'] > previous_create_time
+        previous_create_time = item['attributes']['create_time']
+
+
+def test_mods_sort_by_likes(test_client, mods):
+    response = test_client.get('/mods?order_column=likes')
+
+    assert response.status_code == 200
+    assert response.content_type == 'application/vnd.api+json'
+
+    result = json.loads(response.data.decode('utf-8'))
+    assert len(result['data']) > 0
+
+    previous_create_time = -1
+    for item in result['data']:
+        assert item['attributes']['likes'] > previous_create_time
+        previous_create_time = item['attributes']['likes']
+
+
+def test_mods_sort_by_likes_desc(test_client, mods):
+    response = test_client.get('/mods?order_column=likes&order=desc')
+
+    assert response.status_code == 200
+    assert response.content_type == 'application/vnd.api+json'
+
+    result = json.loads(response.data.decode('utf-8'))
+    assert len(result['data']) > 0
+
+    previous_create_time = sys.maxsize
+    for item in result['data']:
+        assert item['attributes']['likes'] < previous_create_time
+        previous_create_time = item['attributes']['likes']
+
+
+def test_mods_inject_sql_order(test_client):
+    response = test_client.get('/mods?order=or%201=1')
+
+    assert response.status_code == 400
+    assert json.loads(response.get_data(as_text=True))['message'] == 'Invalid order'
+
+
+def test_mods_inject_sql_order_column(test_client):
+    response = test_client.get('/mods?order_column=or%201=1')
+
+    assert response.status_code == 400
+    assert json.loads(response.get_data(as_text=True))['message'] == 'Invalid order column'
 
 
 def test_mods_upload(test_client, app, tmpdir):
