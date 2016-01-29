7c2e8043a37b582a007ca586afc62e4de36bf3cf
Sheeo <sheeo@faforever.com>
2016-01-29 16:29:39 +0100

    Use faf_test

--- init_and_wait_for_db.sh
+++ init_and_wait_for_db.sh
@@ -2,12 +2,13 @@ pushd db
 docker build -t faf-db .
 DB_CONTAINER=`docker run -d --name faf-db -e MYSQL_ROOT_PASSWORD=banana faf-db`
 ./wait_for_db.sh
-docker logs faf-db
-docker exec -i faf-db mysql -h127.0.0.1 -uroot -pbanana < db-structure.sql
-docker exec -i faf-db mysql -h127.0.0.1 -uroot -pbanana < db-data.sql
-popd
 until nc -z $(sudo docker inspect --format='{{.NetworkSettings.IPAddress}}' $DB_CONTAINER) 3306
 do
-  echo "waiting for postgres container..."
+  echo "Waiting for mysql container..."
   sleep 0.5
 done
+docker logs faf-db
+docker exec -i faf-db mysql -h127.0.0.1 -uroot -pbanana -e 'create database faf_test;'
+docker exec -i faf-db mysql -h127.0.0.1 -uroot -pbanana faf_test < db-structure.sql
+docker exec -i faf-db mysql -h127.0.0.1 -uroot -pbanana faf_test < db-data.sql
+popd
