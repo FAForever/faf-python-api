3c6bda51b30465b65e3ba93bedc46a79579e6388
Aaron <avaland@woopta.com>
2016-03-02 21:50:12 -0600

    Added API documentation for the mods endpoint.

--- api/mods.py
+++ api/mods.py
@@ -32,6 +32,29 @@ SELECT_EXPRESSIONS = {
 
 @app.route('/mods/upload', methods=['POST'])
 def mods_upload():
+    """
+    Creates a new mod in the system
+
+    **Example Request**:
+
+    .. sourcecode:: http
+
+       POST /mods/upload
+
+    **Example Response**:
+
+    .. sourcecode:: http
+
+        HTTP/1.1 200 OK
+        Vary: Accept
+        Content-Type: text/javascript
+
+        "ok"
+
+    :query file file: The file submitted (Must be ZIP)
+    :type: zip
+
+    """
     file = request.files.get('file')
     if not file:
         raise InvalidUsage("No file has been provided")
@@ -46,6 +69,45 @@ def mods_upload():
 
 @app.route('/mods/<mod_uid>')
 def mod(mod_uid):
+    """
+    Gets a specific mod definition.
+
+    **Example Request**:
+
+    .. sourcecode:: http
+
+       GET /mods/DF8825E2-DDB0-11DC-90F3-3F9B55D89593
+
+    **Example Response**:
+
+    .. sourcecode:: http
+
+        HTTP/1.1 200 OK
+        Vary: Accept
+        Content-Type: text/javascript
+
+        {
+          "data": {
+            "attributes": {
+              "author": "Sorian(Current) Brent Allen(Original)",
+              "create_time": "2014-04-05T17:13:18+00:00",
+              "description": "Terrain is cratered by heavy ordinance.",
+              "download_url": "http://content.faforever.com/faf/vault/mods/Terrain%20Deform%20for%20FA.v0001.zip",
+              "downloads": 93,
+              "id": "DF8825E2-DDB0-11DC-90F3-3F9B55D89593",
+              "is_ranked": false,
+              "is_ui": false,
+              "likes": 1,
+              "name": "Terrain Deform for FA",
+              "version": "1"
+            },
+            "id": "DF8825E2-DDB0-11DC-90F3-3F9B55D89593",
+            "type": "mod"
+          }
+        }
+
+
+    """
     result = fetch_data(ModSchema(), 'table_mod', SELECT_EXPRESSIONS, MAX_PAGE_SIZE, request,
                         where="`uid` = %s", args=mod_uid, many=False, enricher=enricher)
 
@@ -57,6 +119,48 @@ def mod(mod_uid):
 
 @app.route('/mods')
 def mods():
+    """
+    Lists all mod definitions.
+
+    **Example Request**:
+
+    .. sourcecode:: http
+
+       GET /mods
+
+    **Example Response**:
+
+    .. sourcecode:: http
+
+        HTTP/1.1 200 OK
+        Vary: Accept
+        Content-Type: text/javascript
+
+        {
+          "data": [
+            {
+              "attributes": {
+                "author": "Sorian(Current) Brent Allen(Original)",
+                "create_time": "2014-04-05T17:13:18+00:00",
+                "description": "Terrain is cratered by heavy ordinance.",
+                "download_url": "http://content.faforever.com/faf/vault/mods/Terrain%20Deform%20for%20FA.v0001.zip",
+                "downloads": 93,
+                "id": "DF8825E2-DDB0-11DC-90F3-3F9B55D89593",
+                "is_ranked": false,
+                "is_ui": false,
+                "likes": 1,
+                "name": "Terrain Deform for FA",
+                "version": "1"
+              },
+              "id": "DF8825E2-DDB0-11DC-90F3-3F9B55D89593",
+              "type": "mod"
+            },
+            ...
+          ]
+        }
+
+
+    """
     return fetch_data(ModSchema(), 'table_mod', SELECT_EXPRESSIONS, MAX_PAGE_SIZE, request, enricher=enricher)
 
 
