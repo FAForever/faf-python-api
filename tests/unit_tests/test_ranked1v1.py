f7c8cc5067ead698e473682f899dc34b3ccdf91d
Aaron <avaland@woopta.com>
2016-03-20 17:41:50 -0500

    Added rating tests. (cherry picked from commit 95caad7)

--- tests/unit_tests/test_ranked1v1.py
+++ tests/unit_tests/test_ranked1v1.py
@@ -13,6 +13,7 @@ def ranked1v1_ratings(request, app):
     with db.connection:
         cursor = db.connection.cursor()
         cursor.execute("TRUNCATE TABLE ladder1v1_rating")
+        cursor.execute("TRUNCATE TABLE global_rating")
         cursor.execute("delete from login")
         cursor.execute("""INSERT INTO login
         (id, login, password, email) VALUES
@@ -26,6 +27,12 @@ def ranked1v1_ratings(request, app):
         (2, 2000, 200, 20, 9, 1),
         (3, 1720, 100, 13, 7, 1),
         (4, 1500, 99, 30, 17, 1)""")
+        cursor.execute("""INSERT INTO global_rating
+        (id, mean, deviation, numGames, is_active) VALUES
+        (1, 800, 100, 5, 0),
+        (2, 500, 300, 2, 1),
+        (3, 1600, 200, 3, 1),
+        (4, 1800, 50, 20, 1)""")
 
     def finalizer():
         with db.connection:
@@ -180,3 +187,67 @@ def test_ranked1v1_stats(test_client, ranked1v1_ratings):
 
     result = json.loads(response.data.decode('utf-8'))
     assert result['data']['attributes']['rating_distribution'] == {'1200': 1, '1400': 2}
+
+def test_rating_invalid(test_client, ranked1v1_ratings):
+    response = test_client.get('/rating/')
+
+    assert response.status_code == 404
+
+def test_rating_1v1(test_client, ranked1v1_ratings):
+    response = test_client.get('/rating/1v1')
+
+    assert response.status_code == 200
+    assert response.content_type == 'application/vnd.api+json'
+
+    result = json.loads(response.data.decode('utf-8'))
+    assert 'data' in result
+    assert len(result['data']) == 4
+
+    for item in result['data']:
+        assert 'type' in item
+
+
+def test_rating_global(test_client, ranked1v1_ratings):
+    response = test_client.get('/rating/global')
+    assert response.status_code == 200
+    assert response.content_type == 'application/vnd.api+json'
+
+    result = json.loads(response.data.decode('utf-8'))
+    assert 'data' in result
+    assert len(result['data']) == 4
+
+    for item in result['data']:
+        assert 'type' in item
+
+def test_rating_get_player_invalid(test_client, ranked1v1_ratings):
+    response = test_client.get('/rating/lol/1')
+
+    assert response.status_code == 400
+    assert json.loads(response.get_data(as_text=True))['message'] == 'The rating type should be either 1v1 or global.'
+
+def test_rating_get_player_1v1(test_client, ranked1v1_ratings):
+    response = test_client.get('/rating/1v1/1')
+
+    schema = Ranked1v1Schema()
+
+    result, errors = schema.loads(response.data.decode('utf-8'))
+
+    assert response.status_code == 200
+    assert response.content_type == 'application/vnd.api+json'
+    assert not errors
+    assert result['login'] == 'a'
+    assert result['ranking'] == 1
+
+
+def test_rating_get_player_global(test_client, ranked1v1_ratings):
+    response = test_client.get('/rating/global/1')
+
+    schema = Ranked1v1Schema()
+
+    result, errors = schema.loads(response.data.decode('utf-8'))
+
+    assert response.status_code == 200
+    assert response.content_type == 'application/vnd.api+json'
+    assert not errors
+    assert result['login'] == 'a'
+    assert result['ranking'] == 3
