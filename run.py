adce854c51f5bf084c2e54feffcef3e8b184bc89
Dragonfire <bastian616-git@yahoo.de>
2016-05-13 11:54:52 +0200

    Allow debug with custom ports + more debug logging

--- run.py
+++ run.py
@@ -2,9 +2,8 @@
 """FAForever API server
 
 Usage:
-  server.py
-  server.py --port=80
-  server.py [-d | -aio]
+  run.py
+  run.py [-d | -aio] --port=80
 
 Options:
   -h             Show this screen
@@ -20,11 +19,12 @@ if __name__ == '__main__':
         app.config.from_object('config')
         api_init()
         port = int(args.get("--port"))
-
+        print('listen on port %s' % (port))
         if args.get('-d'):
           app.debug = True
           app.run(host='0.0.0.0', port=port)
         else:
+          print('with aiohttp')
           from aiohttp_wsgi import serve
           from concurrent.futures import ThreadPoolExecutor
           with ThreadPoolExecutor(max_workers=10) as executor:
