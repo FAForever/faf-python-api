c7ff6751139f0712040974dc4e8090e1cc94207e
Dragonfire <bastian616-git@yahoo.de>
2016-05-16 22:20:44 +0200

    Simplify it more

--- sql/clans_refactored.sql
+++ sql/clans_refactored.sql
@@ -5,6 +5,7 @@ CREATE TABLE `clan_list` (
   `clan_name` varchar(40) NOT NULL,
   `clan_tag` varchar(3) DEFAULT NULL,
   `clan_founder_id` mediumint(8) DEFAULT NULL,
+  `clan_leader_id` mediumint(8) DEFAULT NULL,
   `clan_desc` text,
   PRIMARY KEY (`clan_id`)
 );
@@ -16,12 +17,4 @@ CREATE TABLE `clan_members` (
   PRIMARY KEY (`clan_id`,`player_id`),
   KEY `player_id` (`player_id`),
   CONSTRAINT `clan_members_ibfk_1` FOREIGN KEY (`clan_id`) REFERENCES `clan_list` (`clan_id`)
-);
-
-CREATE TABLE `clan_leader` (
-  `clan_id` int(11) NOT NULL,
-  `player_id` int(11) NOT NULL,
-  `became_leader_date` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  PRIMARY KEY (`clan_id`),
-  CONSTRAINT `clan_leader_ibfk_1` FOREIGN KEY (`clan_id`) REFERENCES `clan_list` (`clan_id`)
 );
