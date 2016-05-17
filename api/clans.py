64ff99d34818e217b8fc05c1dcc241948f4550a3
Dragonfire <bastian616-git@yahoo.de>
2016-05-17 03:06:19 +0200

    Make it JSON API

--- api/clans.py
+++ api/clans.py
@@ -1,24 +1,51 @@
 import faf.db as db
 
-from api import *
+from faf.api import ClanSchema
+from flask import request
+from api.query_commons import fetch_data
+from api import app
+
+MAX_PAGE_SIZE = 10000
+
+SELECT_EXPRESSIONS = {
+    'clan_id': 'clan_id',
+    'status': 'status',
+    'clan_name': 'clan_name',
+    'clan_tag': 'clan_tag',
+    'clan_leader_id': 'clan_leader_id',
+    'clan_founder_id': 'clan_founder_id',
+    'clan_desc': 'clan_desc',
+    'create_date': 'create_date'
+}
+
+
+"""CREATE 
+    ALGORITHM = UNDEFINED 
+    DEFINER = `root`@`localhost` 
+    SQL SECURITY DEFINER
+VIEW `clans` AS
+    SELECT 
+        `clan_list`.`clan_id` AS `clan_id`,
+        `clan_list`.`status` AS `status`,
+        `clan_list`.`clan_name` AS `clan_name`,
+        `clan_list`.`clan_tag` AS `clan_tag`,
+        `clan_list`.`clan_leader_id` AS `clan_leader_id`,
+        `clan_list`.`clan_founder_id` AS `clan_founder_id`,
+        `clan_list`.`clan_desc` AS `clan_desc`,
+        `clan_list`.`create_date` AS `create_date`,
+        `leader`.`login` AS `leader_name`,
+        `founder`.`login` AS `founder_name`,
+        (SELECT 
+                COUNT(0)
+            FROM
+                `clan_members`
+            WHERE
+                (`clan_list`.`clan_id` = `clan_members`.`clan_id`)) AS `member_count`
+    FROM
+        ((`clan_list`
+        LEFT JOIN `login` `leader` ON ((`clan_list`.`clan_leader_id` = `leader`.`id`)))
+        LEFT JOIN `login` `founder` ON ((`clan_list`.`clan_founder_id` = `founder`.`id`)))"""
 
 @app.route('/clans')
-def clans_get():
-    clans = []
-    with db.connection.cursor(db.pymysql.cursors.DictCursor) as cursor:
-        cursor.execute('SELECT * FROM clans WHERE status = 1')
-        clans = cursor.fetchall()
-    return { 'data': clans }
-
-@app.route('/clan/<int:id>')
-def clan_get(id):
-    with db.connection.cursor(db.pymysql.cursors.DictCursor) as cursor:
-        cursor.execute('SELECT * FROM clans WHERE clan_id = %s LIMIT 1', id)
-        if cursor.rowcount == 1:
-            clan_details = cursor.fetchall()
-            cursor.execute('SELECT `player_id`, `login`.`login` `player_name`, `join_clan_date` '
-                + 'FROM `clan_members` LEFT JOIN `login` ON `clan_members`.`player_id` = `login`.`id` '
-                + 'WHERE `clan_id` = %s ' , id)
-            return { 'clan_details': clan_details,
-                     'members': cursor.fetchall() }
-    return {}
+def clans():
+    return fetch_data(ClanSchema(), 'clan_list', SELECT_EXPRESSIONS, MAX_PAGE_SIZE, request)
