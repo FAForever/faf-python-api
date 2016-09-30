977b93549b5fca84d4882946f9eb108e8192de69
Michel Jung <michel.jung89@gmail.com>
2016-09-30 20:44:57 +0200

    Switch to db directory before building faf-db

--- init_and_wait_for_db.sh
+++ init_and_wait_for_db.sh
@@ -2,6 +2,5 @@
 pushd db
 ./setup_db.sh
 
-docker build -t faf-db .
-docker exec -i faf-db mysql -h127.0.0.1 -uroot -pbanana faf_test < db-data.sql
+docker exec -i faf-db mysql -uroot -pbanana faf_test < db-data.sql
 popd
