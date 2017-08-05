1ec8706ba18b46f8599533fc59f949d73e8b8b26
Lukas Erlacher <erlacher@in.tum.de>
2017-08-05 11:46:27 +1000

    Add steam link test

--- tests/unit_tests/test_users_route.py
+++ tests/unit_tests/test_users_route.py
@@ -412,7 +412,19 @@ def test_validate_steam_success(test_client, setup_users):
                                                                        'http://faforever.com') + '?openid.identity=http://steamcommunity.com/openid/id/76561198007601820')
 
     assert response.status_code == 302
-    assert response.headers['location'] == 'http://faforever.com?steam_link_result=success'
+    assert 'steam_link_result=success' in response.headers['location']
+
+def test_validate_steam_no_game(test_client, setup_users):
+    #Cannot test without steam api key
+    if not test_client.application.config.get('STEAM_API_KEY'):
+        pytest.skip('Cannot test steam link without api key')
+
+    # uses ID of bot, which does not own game
+    response = test_client.get('/users/validate_steam/' + create_token('link_to_steam', time.time() + 60, 'abc',
+                                                                       'http://faforever.com') + '?openid.identity=http://steamcommunity.com/openid/id/76561198403140265')
+
+    assert response.status_code == 302
+    assert 'steam_link_result=fail' in response.headers['location']
 
 
 def test_change_password_success(oauth, setup_users):
